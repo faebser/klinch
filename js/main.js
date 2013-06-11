57a7ce1b3ad8bc82638ddcab7d884d593f568153
Fabian Frei <fabian.frei@esf-frei.ch>
2013-06-11 21:42:23 +0200

    added mainWrapper and some css fixes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+var ki = (function ($) {
+	// javascript module pattern
+	"use strict"; // enable strict mode for javascript module
+	// private vars
+	var module = {};
+	// private methods
+	// public methods
+	module.init = function () {
+	};
+	//return the module
+	return module;
+}(jQuery));
