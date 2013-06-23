90562a25b22c0af254739481c4d54e5ec3dfacfa
Fabian Frei <fabian.frei@esf-frei.ch>
2013-06-23 20:59:58 +0200

    added js code to center the stuff in the middle, you know it ;) and some small fixes to the index and some css

--- js/main.js
+++ js/main.js
@@ -2,11 +2,28 @@ var ki = (function ($) {
 	// javascript module pattern
 	"use strict"; // enable strict mode for javascript module
 	// private vars
-	var module = {};
+	var module = {},
+	cssCode = null,
+	slider = $("#mainWrapper"),
+	pra = $("article");
 	// private methods
+	var productsInit = function () {
+		//cssCode = $('<style/>').html("#main #mainWrapper article.active {max-width :" + ($(window).width() - 20 * 2)  + "px; }").appendTo($("head"));
+		console.log($(".active").position().left);
+		var pos = $(".active").position().left,
+			width = $(window).width(),
+			w2 = $(".active").width();
+
+			var gna = (width - w2) * 0.5;
+			console.log(gna);
+		slider.css({
+			"marginLeft" : (pos * -1) + gna
+		});
+	};
 	// public methods
 	module.init = function () {
 		// code for placment of buttons
+		productsInit();
 	};
 	//return the module
 	return module;
